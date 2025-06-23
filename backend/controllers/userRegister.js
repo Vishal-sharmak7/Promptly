4fc49327e13282308c22a2ab364f4ccada140338
Vishal-sharmak7 <vishalsharmak7@gmail.com>
2025-06-23 23:06:08 +0530

    .

--- backend/controllers/userRegister.js
+++ backend/controllers/userRegister.js
@@ -1,4 +1,4 @@
-
+import { greetOnMail } from "../middlewares/nodemailer.js";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
@@ -7,29 +7,34 @@ const userRegister = async (req, res) => {
     const { email, name, password } = req.body;
 
     if (!email || !name || !password) {
-      return res.ststus(400).json({ success: false, message: "Auth Fail" });
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
     }
 
     const userExist = await User.findOne({ email });
     if (userExist) {
-      res.status(400).json({ message: "user already exist" });
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
     }
 
     const hashpassword = await bcrypt.hash(password, 10);
-    console.log(hashpassword);
-    
-    const newUser = new User({
-      email,
-      name,
-      password:hashpassword,
-    
-    });
-    await newUser.save();
-  
-    res.status(201).json({ message: "user register successfully" ,newUser});
-  } catch (error) {}
 
-  return res.status(500).json({ message: "server error" });
+    const newUser = new User({ email, name, password: hashpassword });
+    const savedUser = await newUser.save();
+
+    greetOnMail({ name, email });
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: savedUser,
+    });
+  } catch (error) {
+    console.error("Registration Error:", error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
 };
 
 export default userRegister;
